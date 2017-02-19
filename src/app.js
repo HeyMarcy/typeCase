6ade260bdac2593461386327242ef760de246b00
marcy <marcy.montross@gmail.com>
2017-02-19 16:37:10 -0600

    installed font-awesome-icons

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const express = require('express');
 
 const app = express();
 
+const faIcons = require('font-awesome-icons');
+
+
+
 app.get('/', (req, res)=>{
   res.send('my app is running!')
 });
@@ -14,3 +18,7 @@ app.listen(8080, () => {
 });
 
 // const URL = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAowurJ1GrZKbUpmwcs8v0oaC8BWDKwsfg";
+
+
+//faIcons.getList().then(icons => console.log(icons.length));
+//faIcons.getList().then(icons => console.log(icons[0]));
